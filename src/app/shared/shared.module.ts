0258a3c5726e4ce1fdb26616a058376dd4210a90
dsaurabhkumar <dsaurabh.kumar1288@gmail.com>
2018-09-17 21:34:54 +0530

    Google ads component generated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GstWidgetContainerComponent } from './gst-widget-container/gst-widget-container.component';
+import { GoogleAdsComponent } from './google-ads/google-ads.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [GstWidgetContainerComponent],
-  declarations: [GstWidgetContainerComponent]
+  exports: [GstWidgetContainerComponent, GoogleAdsComponent],
+  declarations: [GstWidgetContainerComponent, GoogleAdsComponent]
 })
 export class SharedModule { }
