9f593979e4b9db20b290a4212855286663b87fe9
digvijay442 <digvijay442@gmail.com>
2018-09-18 13:27:57 +0530

    confilct fixed

--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsBaseComponent } from './settings-base/settings-base.component';
 import { SharedModule } from '@app/shared/shared.module';
+import { MembershipDetailsComponent } from '@app/pages/settings/membership-details/membership-details.component';
 
 @NgModule({
   imports: [
@@ -11,6 +12,6 @@ import { SharedModule } from '@app/shared/shared.module';
     SettingsRoutingModule,
     SharedModule
   ],
-  declarations: [SettingsBaseComponent]
+  declarations: [SettingsBaseComponent, MembershipDetailsComponent]
 })
 export class SettingsModule { }
