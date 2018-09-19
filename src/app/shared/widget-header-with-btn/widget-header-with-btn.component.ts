3acaffb9ce7d758503a51b5b82c2a4b8b89a2151
dsaurabhkumar <dsaurabh.kumar1288@gmail.com>
2018-09-19 17:34:09 +0530

    Customer module updated

---
+++ src/app/shared/widget-header-with-btn/widget-header-with-btn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-widget-header-with-btn',
+  templateUrl: './widget-header-with-btn.component.html',
+  styleUrls: ['./widget-header-with-btn.component.scss']
+})
+export class WidgetHeaderWithBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
