735ce2a9a90b945e9c2ebd94c16f9f64eebff664
dsaurabhkumar <dsaurabh.kumar1288@gmail.com>
2018-09-25 15:33:50 +0530

    Customer module updated

--- src/app/pages/customers/add-new/add-new.component.ts
+++ src/app/pages/customers/add-new/add-new.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTES } from '@app/config/route.constant';
-
+import { CoreConstant } from '@app/config/core.constant';
 @Component({
   selector: 'app-add-new',
   templateUrl: './add-new.component.html',
   styleUrls: ['./add-new.component.scss']
 })
 export class AddNewComponent implements OnInit {
+
+  countries = CoreConstant.countries;
+  states = CoreConstant.states;
+  customers = CoreConstant.customers;
 
   constructor(
     private _router: Router
