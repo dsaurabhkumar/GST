8a7c69bf4e9c44f3b94b7ba8244ed06008489669
digvijay442 <digvijay442@gmail.com>
2018-09-28 10:47:20 +0530

    update to edit invoice

--- src/app/pages/settings/edit-invoice/edit-invoice.component.ts
+++ src/app/pages/settings/edit-invoice/edit-invoice.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ROUTES } from '@app/config/route.constant';
 
 @Component({
   selector: 'app-edit-invoice',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditInvoiceComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _router: Router
+  ) { }
 
   ngOnInit() {
   }
+
+  backToSettings(){
+    this._router.navigate([ROUTES.settings.absoluteRoute])
+  }
+
 
 }
--- src/app/pages/settings/edit-user/edit-user.component.ts
+++ src/app/pages/settings/edit-user/edit-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ROUTES } from '@app/config/route.constant';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-user',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  backToSettings(){
+    this._router.navigate([ROUTES.settings.absoluteRoute])
   }
 
 }
