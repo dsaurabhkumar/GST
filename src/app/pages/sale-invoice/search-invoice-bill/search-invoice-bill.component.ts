db075ddba32997597a1b08c0c8e063649255aa0b
dsaurabhkumar <dsaurabh.kumar1288@gmail.com>
2018-10-11 16:17:27 +0530

    Sale-invoice module

---
+++ src/app/pages/sale-invoice/search-invoice-bill/search-invoice-bill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-invoice-bill',
+  templateUrl: './search-invoice-bill.component.html',
+  styleUrls: ['./search-invoice-bill.component.scss']
+})
+export class SearchInvoiceBillComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
