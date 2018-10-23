d2a2da18e689b4eae99e61d45207c63627033c6c
dsaurabhkumar <dsaurabh.kumar1288@gmail.com>
2018-10-23 10:38:38 +0530

    Sale-Invoice Module

--- src/app/pages/sale-invoice/sale-invoice.module.ts
+++ src/app/pages/sale-invoice/sale-invoice.module.ts
@@ -5,6 +5,8 @@ import { SaleInvoiceRoutingModule } from './sale-invoice-routing.module';
 import { SaleInvoiceBaseComponent } from './sale-invoice-base/sale-invoice-base.component';
 import { SearchInvoiceBillComponent } from './search-invoice-bill/search-invoice-bill.component';
 import { SharedModule } from '@app/shared/shared.module';
+import { InvoiceSummaryComponent } from './invoice-summary/invoice-summary.component';
+import { InvoiceListComponent } from './invoice-list/invoice-list.component';
 
 @NgModule({
   imports: [
@@ -12,6 +14,6 @@ import { SharedModule } from '@app/shared/shared.module';
     SaleInvoiceRoutingModule,
     SharedModule
   ],
-  declarations: [SaleInvoiceBaseComponent, SearchInvoiceBillComponent]
+  declarations: [SaleInvoiceBaseComponent, SearchInvoiceBillComponent, InvoiceSummaryComponent, InvoiceListComponent]
 })
 export class SaleInvoiceModule { }
