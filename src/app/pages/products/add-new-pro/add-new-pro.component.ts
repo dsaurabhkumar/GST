d701de347dd33c3903890446bfbe8b4d03cedefd
digvijay442@gmail.com <digvijay442@gmail.com>
2018-10-25 14:49:15 +0530

    responsive nav update

--- src/app/pages/products/add-new-pro/add-new-pro.component.ts
+++ src/app/pages/products/add-new-pro/add-new-pro.component.ts
@@ -1,26 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { CoreConstant } from '@app/config/core.constant';
-import { Router } from '@angular/router';
-import { ROUTES } from '@app/config/route.constant';
+import { Component, OnInit } from "@angular/core";
+import { CoreConstant } from "@app/config/core.constant";
+import { Router } from "@angular/router";
+import { ROUTES } from "@app/config/route.constant";
 
 @Component({
-  selector: 'app-add-new-pro',
-  templateUrl: './add-new-pro.component.html',
-  styleUrls: ['./add-new-pro.component.scss']
+  selector: "app-add-new-pro",
+  templateUrl: "./add-new-pro.component.html",
+  styleUrls: ["./add-new-pro.component.scss"]
 })
 export class AddNewProComponent implements OnInit {
-
   measurementUnits = CoreConstant.measurementUnits;
 
-  constructor(
-    private _router: Router
-  ) { }
+  constructor(private _router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   backToProductBase() {
-    this._router.navigate([ROUTES.products.absoluteRoute]);
+    this._router.navigate([ROUTES.products.absoluteRoute]
+      );
   }
-
 }
