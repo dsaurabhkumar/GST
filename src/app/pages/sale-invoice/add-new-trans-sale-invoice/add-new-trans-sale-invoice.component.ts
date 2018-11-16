0fbdb7ec9f0282f52785a20f44c3c484976073ec
dsaurabhkumar <dsaurabh.kumar1288@gmail.com>
2018-11-16 15:48:47 +0530

    Validation of Sale Invoice module completed

--- src/app/pages/sale-invoice/add-new-trans-sale-invoice/add-new-trans-sale-invoice.component.ts
+++ src/app/pages/sale-invoice/add-new-trans-sale-invoice/add-new-trans-sale-invoice.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CoreConstant } from '@app/config/core.constant';
+import { Validators,FormGroup, FormControl } from '@angular/forms';
+import { CommonUtilService } from '@app/core/services/common-util.service';
 
 @Component({
   selector: 'app-add-new-trans-sale-invoice',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-new-trans-sale-invoice.component.scss']
 })
 export class AddNewTransSaleInvoiceComponent implements OnInit {
+  addTransForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private _commonUtil: CommonUtilService
+  ) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+  onSubmitAddTransForm(form){
+    this._commonUtil.validateForm(form);
+    if(form.invalid)
+    return;
+  }
+  createForm(){
+    this.addTransForm = new FormGroup({
+      'transName': new FormControl('', [Validators.required, Validators.pattern(CoreConstant.pattern.letter)]),
+    })
   }
 
 }
