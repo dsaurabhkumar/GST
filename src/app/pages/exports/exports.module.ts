79444e956facecfc338b91f90b2cd00a18ae01d2
digvijay442@gmail.com <digvijay442@gmail.com>
2018-11-16 12:03:11 +0530

    export module , floating contact

--- src/app/pages/exports/exports.module.ts
+++ src/app/pages/exports/exports.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ExportsRoutingModule } from './exports-routing.module';
 import { ExportsBaseComponent } from './exports-base/exports-base.component';
+import { SharedModule } from '@app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ExportsRoutingModule
+    ExportsRoutingModule,
+    SharedModule
   ],
   declarations: [ExportsBaseComponent]
 })
